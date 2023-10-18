55015d625975a07c3f65cf40c0f785c66ea8ec3f
KarlJohanAkesson <ka1334ak-s@student.lu.se>
2023-10-18 16:50:39 +0200

    Kalle listor uptadering 2

--- openlibrary-app/src/Lists.js
+++ openlibrary-app/src/Lists.js
@@ -1,12 +1,14 @@
 import { useState } from 'react';
-import './Lists.css';
 import Makelist from './Makelist.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import Stack from 'react-bootstrap/Stack';
 
 function Lists(){
     //temporary list of books to be displayed
     const temp = [
         {"Title":"Dune", "Author": "Frank Herbert", "Year": 1965, "Publisher":"Chilton Books"},
-        {"Title":"Metro 2033", "Author": "Dmitry Glukhovsky", "Year": 2002, "Publisher":"Eksmo"}
+        {"Title":"Metro 2033", "Author": "Dmitry Glukhovsky", "Year": 2002, "Publisher":"Eksmo"},
+        {"Title":"The Big Short", "Author": "Michael Lewis", "Year": 2010, "Publisher":"W. W. Norton & Company"}
     ];
     //assigning the list of books to different tables, they are currently the same for testing purposes
     const [favList, setFavList] = useState(temp);
@@ -15,9 +17,11 @@ function Lists(){
 
     return (
         <div>
-            <Makelist list={favList} position={"left"} title={"Favorite Books"}/>
-            <Makelist list={toReadList} position={"center"} title={"Books to read"}/>
-            <Makelist list={finList} position={"right"} title={"Books read"}/>
+            <Stack direction="horizontal" gap={5}>
+                <Makelist list={favList} setList={setFavList} position={"left"} title={"Favorite Books"}/>
+                <Makelist list={toReadList} setList={setToReadList} position={"center"} title={"Books to read"}/>
+                <Makelist list={finList} setList={setFinList} position={"right"} title={"Books read"}/>
+            </Stack>
         </div>
     )
 }
--- openlibrary-app/src/Makelist.js
+++ openlibrary-app/src/Makelist.js
@@ -1,24 +1,50 @@
-function Makelist({list, position, title}){
+import Table from 'react-bootstrap/Table';
+import 'bootstrap/dist/css/bootstrap.css';
+import Button from 'react-bootstrap/Button';
+
+
+function Makelist({list, setList, title}){
+
+    let temp = [...list];
+    if(list.length === 0){
+        temp = [{"Title":"Title", "Author": "Author", "Year": "Year", "Publisher":"Publisher"}]
+    }
+
+    const handleRemoval = (e, row) => {
+        console.log(list);
+        if(list.length>1){
+            setList(list => {
+                return list.filter(book => book.Title !== row.Title)
+            })
+        } else {
+            setList([]);
+        }
+
+    };
+
+
     return (
-        <div className = "fixed_header">
-        <h2 className={position + "head"}>{title}</h2>
-            <table className={position} id={title}>
+        <div>
+        <h2>{title}</h2>
+            <Table responsive striped bordered hover size="sm" variant='dark' id={title}>
                 <thead>
                     <tr key={title}>
-                        {Object.keys(list[0]).map(obj => 
-                            <th>{obj}</th>
+                        {Object.keys(temp[0]).map(obj => 
+                            <th key={obj}>{obj}</th>
                         )}
+                        <th key={"button"}></th>
                     </tr>
                 </thead>
                 <tbody>
-                    {list.map(obj=> 
+                    {temp.map(obj=> 
                         <tr key={obj["Title"]} >
                             {Object.values(obj).map(obj2 => 
-                                <td>{obj2}</td>
+                                <td key={obj2}>{obj2}</td>
                             )}
+                            <td key={"button"} value={obj}>{<Button variant="secondary" size="sm" disabled={list.length === 0} onClick={e=>handleRemoval(e, obj)}>"Remove book"</Button>}</td>
                 </tr>)}
                 </tbody>
-            </table>
+            </Table>
         </div>
     )
 }
