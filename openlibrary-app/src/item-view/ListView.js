d74078472ad7224769254839d118d6cf97def356
martin <martin@mandella.se>
2023-10-19 17:26:40 +0200

    responsive listsview med temp. listor

--- openlibrary-app/src/item-view/ListView.js
+++ openlibrary-app/src/item-view/ListView.js
@@ -3,20 +3,32 @@ import Button from "react-bootstrap/Button";
 import ListGroup from "react-bootstrap/ListGroup";
 import Card from "react-bootstrap/Card"
 import Stack from "react-bootstrap/Stack";
+import { useState } from "react";
 
-const lists = [];
+const lists = [{"name": "favoriter1", "list_items":["abc", "dfe", "blabla", "herkules"]},
+ {"name": "pingviiner", "list_items":["poirot", "köttbulle", "falukorv"]}, 
+ {"name": "lunch", "list_items": ["fiskpinne", "potatis"]}];
+
 
 function ListView() {
+  const [index, setIndex] = useState(0);
+
   return (<div>
     <Stack className="container" direction="horizontal">
       <div className="list-list p-2">
         <ListGroup variant="flush">
           {
             //for each list, make a listgroup.item, usestate with index to choose which list to render
+            lists.map(list => {
+              return (
+                <ListGroup.Item className="list-item" 
+                  onClick={e => setIndex(lists.indexOf(list))}
+                  key={list.name}>
+                    {list.name}
+                </ListGroup.Item>
+              )
+            })
           }
-          <ListGroup.Item className="list-item">list object 1</ListGroup.Item>
-          <ListGroup.Item className="list-item">Dapibus ac facilisdis in</ListGroup.Item>
-          <ListGroup.Item className="list-item">Vestibulum at eros</ListGroup.Item>
         </ListGroup>
       </div>
       <div className="list-container p-2">
@@ -24,10 +36,13 @@ function ListView() {
           <Card.Title className="list-card-title me-auto">List name</Card.Title>
           <Card.Body className="list-description">List description that should be somewhat long i guess but not too long because the main thing is the list</Card.Body>
           <ListGroup variant="flush">
-            {/* for the chosen list (index in usestate) generate listitems for all items */}
-            <ListGroup.Item className="list-item">list object 1</ListGroup.Item>
-            <ListGroup.Item className="list-item">Dapibus ac facilisis in</ListGroup.Item>
-            <ListGroup.Item className="list-item">Vestibulum at eros</ListGroup.Item>
+            {/* for the chosen list (index in usestate) generate listitems for all items */
+              lists[index].list_items.map(item => {
+                return (
+                  <ListGroup.Item className="list-item" key={item}>{item}</ListGroup.Item>
+                )
+              })
+            }
           </ListGroup>
         </Card>
       </div>
